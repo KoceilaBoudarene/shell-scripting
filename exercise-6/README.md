7d3463238d0922b8e5dc2a7aa5d1cee1113cc9b9
Koceila Boudarene <koceila.boudarene@mail.com>
2021-12-12 19:10:03 +0100

    Exercice 6

---
+++ exercise-6/README.md
@@ -0,0 +1,3 @@
+Utilisation de man suivi de mkdir pour trouver l'option -p, le reste étant des choses vu en cours ( cd et touch ).
+
+-p permet dans un premier temps de ne pas afficher d'erreur si le fichier existe déjà, et de faire un dir via l'adresse de sa location.
---
+++ exercise-6/my_ansible.sh
@@ -0,0 +1,10 @@
+mkdir -p /home/koceila/documents
+touch meeting_notes.txt
+mkdir -p /home/koceila/documents/gamez -p /home/koceila/documents/images -p /home/koceila/documents/work
+mkdir -p /home/koceila/documents/work/code -p /home/koceila/documents/work/plannings
+cd /home/koceila/documents/work/plannings
+touch april.xlsx february.xlsx january.xlsx march.xlsx
+cd /home/koceila/documents/gamez
+touch csgo.exe
+chmod 764 csgo.exe
+exit 0
