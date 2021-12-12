e1cb732c547a08ca8de60b3dad74def34575eb0f
Koceila Boudarene <koceila.boudarene@mail.com>
2021-12-12 19:04:24 +0100

    Exercice 5

---
+++ exercise-5/README.md
@@ -0,0 +1,3 @@
+Le même code que salutation.sh mais avec un elif ainsi qu'une nouvelle condition
+
+str$1 = str$2 vérifie si le premier argument est le même que le second, dans ce cas, le script affichera le message suivant "Firstname and lastname cannot be the same!"
---
+++ exercise-5/salutation_advanced.sh
@@ -0,0 +1,10 @@
+if [ -z "$1" ] || [ -z "$2" ] || [ "$#" -ne 2 ]; then
+  echo "Usage ./salutation.sh firstname lastname."
+  exit 1
+elif [ str$1 = str$2 ]; then
+  echo "Firstname and lastname cannot be the same!"
+  exit 2
+else
+  echo "Hello $1 $2 , have a nice day!"
+  exit 0
+fi
